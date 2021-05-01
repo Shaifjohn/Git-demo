dbf6e5eb21f27c462d833249a8e17cb3129eea67
Shaifjohn <83347476+Shaifjohn@users.noreply.github.com>
2021-05-01 13:17:58 +0200

    InleidingJava

---
+++ hoofdstuk2/HoofdStukTwee.java
@@ -0,0 +1,18 @@
+
+import java.awt.*;
+import java.applet.*;
+
+    //een klasse met de naam Show van het type Applet
+    public class HoofdStukTwee extends Applet {
+
+        //een (lege) methode die de Applet initialiseert
+        public void init() {
+
+        }
+        //een methode die de inhoud van het scherm tekent
+        public void paint(Graphics g) {
+            g.drawString("Welcome to Java!!", 50, 60 );
+        }
+
+    }
+}
